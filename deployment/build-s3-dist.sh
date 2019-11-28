e4222319f8f2ac2527626c6e5bb9d1e2a95acfe5
Andrey Gorbunov <andrey@optimuspro.ru>
2019-11-29 01:29:00 +0300

    Tweak the build script & template - remove extra resources

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -1,25 +1,27 @@
+#!/bin/bash
+
 echo "------------------------------------------------------------------------------"
 echo "Setup the dist folder"
 echo "------------------------------------------------------------------------------"
 rm -r dist 
 mkdir dist
 cd dist
-mkdir demo-ui
+#mkdir demo-ui
 cd ..
 
 echo "------------------------------------------------------------------------------"
 echo "Copy in the template"
 echo "------------------------------------------------------------------------------"
-cp *.template dist/ 
+cp *.yaml dist/ 
  
 replace="s/%%BUCKET_NAME%%/$1/g" 
-sed -i '' -e $replace dist/*.template 
+sed -i -e $replace dist/*.yaml
  
-replace="s/%%TEMPLATE_BUCKET%%/$2/g" 
-sed -i '' -e $replace dist/*.template 
+#replace="s/%%TEMPLATE_BUCKET%%/$2/g" 
+#sed -i '' -e $replace dist/*.template 
  
-replace="s/%%VERSION%%/$3/g" 
-sed -i '' -e $replace dist/*.template 
+replace="s/%%VERSION%%/$2/g" 
+sed -i -e $replace dist/*.yaml 
  
 cd ../source 
 
@@ -30,24 +32,25 @@ cd image-handler
 npm install 
 npm run build 
 cp dist/image-handler.zip ../../deployment/dist/image-handler.zip 
-
-echo "------------------------------------------------------------------------------"
-echo "Package the demo-ui assets"
-echo "------------------------------------------------------------------------------"
 cd ..
-cp -r ./demo-ui/** ../deployment/dist/demo-ui
 
-echo "------------------------------------------------------------------------------"
-echo "Package the custom-resource code"
-echo "------------------------------------------------------------------------------"
-cd custom-resource
-npm install 
-npm run build 
-cp dist/custom-resource.zip ../../deployment/dist/custom-resource.zip 
+#echo "------------------------------------------------------------------------------"
+#echo "Package the demo-ui assets"
+#echo "------------------------------------------------------------------------------"
 
-echo "------------------------------------------------------------------------------"
-echo "Generate the demo-ui manifest document"
-echo "------------------------------------------------------------------------------"
-cd ../../deployment/manifest-generator
-npm install
-node app.js --target ../../source/demo-ui --output ../dist/demo-ui-manifest.json
+#cp -r ./demo-ui/** ../deployment/dist/demo-ui
+
+# echo "------------------------------------------------------------------------------"
+# echo "Package the custom-resource code"
+# echo "------------------------------------------------------------------------------"
+# cd custom-resource
+# npm install 
+# npm run build 
+# cp dist/custom-resource.zip ../../deployment/dist/custom-resource.zip 
+
+# echo "------------------------------------------------------------------------------"
+# echo "Generate the demo-ui manifest document"
+# echo "------------------------------------------------------------------------------"
+# cd ../../deployment/manifest-generator
+# npm install
+# node app.js --target ../../source/demo-ui --output ../dist/demo-ui-manifest.json
