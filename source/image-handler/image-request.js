5fab8e672036c1bc3bdd98c562673e411a145062
Andrey Gorbunov <andrey@optimuspro.ru>
2020-04-11 12:59:18 +0300

    Merge with master branch

--- source/image-handler/image-request.js
+++ source/image-handler/image-request.js
@@ -1,12 +1,12 @@
 /*********************************************************************************************************************
  *  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
  *                                                                                                                    *
- *  Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance        *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
  *  with the License. A copy of the License is located at                                                             *
  *                                                                                                                    *
- *      http://aws.amazon.com/asl/                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
  *                                                                                                                    *
- *  or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
  *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
  *  and limitations under the License.                                                                                *
  *********************************************************************************************************************/
@@ -14,7 +14,7 @@
 const ThumborMapping = require('./thumbor-mapping');
 
 class ImageRequest {
-    
+
     /**
      * Initializer function for creating a new image request, used by the image
      * handler to perform image modifications.
@@ -26,7 +26,36 @@ class ImageRequest {
             this.bucket = this.parseImageBucket(event, this.requestType);
             this.key = this.parseImageKey(event, this.requestType);
             this.edits = this.parseImageEdits(event, this.requestType);
-            this.originalImage = await this.getOriginalImage(this.bucket, this.key)
+            this.originalImage = await this.getOriginalImage(this.bucket, this.key);
+
+            /* Decide the output format of the image.
+             * 1) If the format is provided, the output format is the provided format.
+             * 2) If headers contain "Accept: image/webp", the output format is webp.
+             * 3) Use the default image format for the rest of cases.
+             */
+            let outputFormat = this.getOutputFormat(event, this.edits);
+            if (this.edits && this.edits.toFormat) {
+                this.outputFormat = this.edits.toFormat;
+            } else if (outputFormat) {
+                this.outputFormat = outputFormat;
+            }
+
+            // Fix quality for Thumbor and Custom request type if outputFormat is different from quality type.
+            if (this.outputFormat) {
+                const requestType = ['Custom', 'Thumbor'];
+                const acceptedValues = ['jpeg', 'png', 'webp', 'tiff', 'heif'];
+
+                this.ContentType = `image/${this.outputFormat}`;
+                if (requestType.includes(this.requestType) && acceptedValues.includes(this.outputFormat)) {
+                    let qualityKey = Object.keys(this.edits).filter(key => acceptedValues.includes(key))[0];
+                    if (qualityKey && (qualityKey !== this.outputFormat)) {
+                        const qualityValue = this.edits[qualityKey];
+                        this.edits[this.outputFormat] = qualityValue;
+                        delete this.edits[qualityKey];
+                    }
+                }
+            }
+
             return Promise.resolve(this);
         } catch (err) {
             return Promise.reject(err);
@@ -43,17 +72,36 @@ class ImageRequest {
         const S3 = require('aws-sdk/clients/s3');
         const s3 = new S3();
         const imageLocation = { Bucket: bucket, Key: key };
-        const request = s3.getObject(imageLocation).promise();
         try {
-            const originalImage = await request;
+            const originalImage = await s3.getObject(imageLocation).promise();
+
+            if (originalImage.ContentType) {
+                this.ContentType = originalImage.ContentType;
+            } else {
+                this.ContentType = "image";
+            }
+
+            if (originalImage.Expires) {
+                this.Expires = new Date(originalImage.Expires).toUTCString();
+            }
+
+            if (originalImage.LastModified) {
+                this.LastModified = new Date(originalImage.LastModified).toUTCString();
+            }
+
+            if (originalImage.CacheControl) {
+                this.CacheControl = originalImage.CacheControl;
+            } else {
+                this.CacheControl = "max-age=31536000,public";
+            }
+
             return Promise.resolve(originalImage.Body);
-        }
-        catch(err) {
+        } catch(err) {
             return Promise.reject({
-                status: 500,
+                status: ('NoSuchKey' === err.code) ? 404 : 500,
                 code: err.code,
                 message: err.message
-            })
+            });
         }
     }
 
@@ -74,7 +122,7 @@ class ImageRequest {
             if (bucket !== "") {
                 // Check the provided bucket against the whitelist
                 const sourceBuckets = this.getAllowedSourceBuckets();
-                if (sourceBuckets.includes(bucket)) {
+                if (sourceBuckets.includes(bucket) || bucket.match(new RegExp('^' + sourceBuckets[0] + '$'))) {
                     return bucket;
                 } else {
                     throw ({
@@ -90,7 +138,7 @@ class ImageRequest {
             }
         } else {
             throw ({
-                status: 400,
+                status: 404,
                 code: 'ImageBucket::CannotFindBucket',
                 message: 'The bucket you specified could not be found. Please check the spelling of the bucket name in your request.'
             });
@@ -148,7 +196,7 @@ class ImageRequest {
         if (key === "") {
             // Return an error for all other conditions
             throw ({
-                status: 400,
+                status: 404,
                 code: 'ImageEdits::CannotFindImage',
                 message: 'The image you specified could not be found. Please check your request syntax as well as the bucket you specified to ensure it exists.'
             });
@@ -159,7 +207,7 @@ class ImageRequest {
 
     /**
      * Determines how to handle the request being made based on the URL path
-     * prefix to the image request. Categorizes a request as either "image" 
+     * prefix to the image request. Categorizes a request as either "image"
      * (uses the Sharp library), "thumbor" (uses Thumbor mapping), or "custom"
      * (uses the rewrite function).
      * @param {Object} event - Lambda request body.
@@ -184,9 +232,10 @@ class ImageRequest {
             return undefined;
         }
 
-        const toBuffer = new Buffer(encoded, 'base64');
+        const toBuffer = Buffer.from(encoded, 'base64');
         try {
-            return JSON.parse(toBuffer.toString('ascii'));
+            // To support European characters, 'ascii' was removed.
+            return JSON.parse(toBuffer.toString());
         } catch (e) {
             throw ({
                 status: 400,
@@ -197,7 +246,7 @@ class ImageRequest {
     }
 
     /**
-     * Returns a formatted image source bucket whitelist as specified in the 
+     * Returns a formatted image source bucket whitelist as specified in the
      * SOURCE_BUCKETS environment variable of the image handler Lambda
      * function. Provides error handling for missing/invalid values.
      */
@@ -214,6 +263,22 @@ class ImageRequest {
             const buckets = formatted.split(',');
             return buckets;
         }
+    }
+
+    /**
+    * Return the output format depending on the accepts headers and request type
+    * @param {Object} event - The request body.
+    * @param {Object} edits - The list of edits.
+    */
+    getOutputFormat(event, edits) {
+        const autoWebP = process.env.AUTO_WEBP;
+        if (autoWebP && event.headers.Accept && event.headers.Accept.includes('image/webp')) {
+            return 'webp';
+        } else if (this.requestType === 'Default') {            
+            return edits.outputFormat;
+        }
+
+        return null;
     }
 }
 
